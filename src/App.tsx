f3a4f5cd56989fb8bb7501fac03972ac8a8efd97
Alyona Pisotska <pisotska.o@gmail.com>
2025-04-19 20:40:30 +0300

    init: + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './index.css';
+import "./index.css";
 
 function App() {
-  return <div className="text-red-500 bg-red-300" >Music app</div>
+  return <div className="text-red-500 bg-red-300">Music app</div>;
 }
 
-export default App
+export default App;
