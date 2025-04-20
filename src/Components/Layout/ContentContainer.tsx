6489c8de3239c08151c840c4780e5f2f229a3729
Alyona Pisotska <pisotska.o@gmail.com>
2025-04-20 13:01:00 +0300

    refactor: + styles

--- src/Components/Layout/ContentContainer.tsx
+++ src/Components/Layout/ContentContainer.tsx
@@ -2,7 +2,7 @@ import { TrackToolbar } from '@/Components/components.ts';
 
 const ContentContainer = () => {
   return (
-    <div className='h-full rounded-[4px] bg-white p-4'>
+    <div className='h-full rounded-[4px] bg-white p-6'>
       <TrackToolbar />
     </div>
   );
--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { ContentContainer } from '@/Components/Layout/ContentContainer.tsx';
 
 const Layout = () => {
   return (
-    <main className='bg-accent/10 h-screen px-50 py-5'>
+    <main className='bg-accent/10 h-screen px-50 py-8'>
       <ContentContainer />
     </main>
   );
