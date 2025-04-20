924acee09f3b000964921576035913fbfb2b2ed7
Alyona Pisotska <pisotska.o@gmail.com>
2025-04-20 12:34:00 +0300

    feat: + TrackToolbar component with search input and create button

--- src/Components/Layout/ContentContainer.tsx
+++ src/Components/Layout/ContentContainer.tsx
@@ -1,5 +1,11 @@
+import { TrackToolbar } from '@/Components/components.ts';
+
 const ContentContainer = () => {
-  return <div className='h-full rounded-[4px] bg-white p-4'>ContentContainer</div>;
+  return (
+    <div className='h-full rounded-[4px] bg-white p-4'>
+      <TrackToolbar />
+    </div>
+  );
 };
 
 export { ContentContainer };
--- src/Components/components.ts
+++ src/Components/components.ts
@@ -1,4 +1,8 @@
 export { Layout } from './Layout/Layout.tsx';
+export { Search } from './Search/Search.tsx';
+export { CreateTrackButton } from './CreateTrackButton/CreateTrackButton.tsx';
+export { TrackToolbar } from './TrackToolbar/TrackToolbar.tsx';
 
 /** shad-cn components */
 export { Button, buttonVariants } from './ui/button.tsx';
+export { Input } from './ui/input.tsx';
