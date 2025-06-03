bdd5d9259ebc6e00a79c7c476218a6b0a62f5fae
Alyona Pisotska <pisotska.o@gmail.com>
2025-06-03 12:34:32 +0300

    refactor: * error handling for Axios responses. Parsed server-provided error messages

--- src/lib/utils/handle-axios-error.ts
+++ src/lib/utils/handle-axios-error.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+type ServerError = {
+  error: string;
+};
+
 const handleAxiosError = (e: unknown): Error => {
-  if (axios.isAxiosError(e)) {
-    const message = e.message || 'Axios error';
+  if (axios.isAxiosError<ServerError>(e)) {
+    const errorData = e.response?.data;
+
+    const message =
+      typeof errorData === 'object' && errorData !== null && 'error' in errorData
+        ? errorData.error
+        : e.message || 'Axios error';
 
     return new Error(message);
   }
