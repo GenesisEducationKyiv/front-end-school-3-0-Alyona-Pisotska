e90da5fe5a57975d4490f00e2cbd7dab4be85d91
Alyona Pisotska <pisotska.o@gmail.com>
2025-06-26 16:49:37 +0300

    feat: + integrate `rollup-plugin-visualizer` for bundle analysis and add `.gitignore` entry for reports

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    visualizer({
+      open: true,
+      filename: 'bundle-report/bundle-report.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   server: {
     port: 3000,
   },
