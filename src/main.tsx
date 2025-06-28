2802dec962f227772fcc379ee4e5ea81fd132b67
Alyona Pisotska <pisotska.o@gmail.com>
2025-06-28 13:06:59 +0300

    feat: + integrate Sentry for error tracking, add sourcemap removal during build

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import { SearchTextContextProvider, GenreContextProvider, QueryParamsProvider } from '@/contexts/contexts';
 
+import '@/integrations/sentry';
 import './index.css';
 
 const queryClient = new QueryClient();
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,52 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, loadEnv, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    tsconfigPaths(),
-    visualizer({
-      open: true,
-      filename: 'bundle-report/bundle-report.html',
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
-  server: {
-    port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    exclude: ['node_modules/', 'tests/', 'playwright/', 'playwright-report/', '**/*.spec.tsx'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/', 'test/', '**/*.test.ts', '**/*.spec.ts'],
+import packageJson from './package.json';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      __APP_VERSION__: JSON.stringify(packageJson.version),
     },
-  },
+    plugins: [
+      react(),
+      tailwindcss(),
+      tsconfigPaths(),
+      visualizer({
+        open: true,
+        filename: 'bundle-report/bundle-report.html',
+        gzipSize: true,
+        brotliSize: true,
+      }),
+      sentryVitePlugin({
+        org: env.VITE_SENTRY_ORG,
+        project: env.VITE_SENTRY_PROJECT,
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+        telemetry: false,
+      }) as PluginOption,
+    ],
+    server: {
+      port: 3000,
+    },
+    build: {
+      sourcemap: 'hidden',
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+      exclude: ['node_modules/', 'tests/', 'playwright/', 'playwright-report/', '**/*.spec.tsx'],
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+        exclude: ['node_modules/', 'test/', '**/*.test.ts', '**/*.spec.ts'],
+      },
+    },
+  };
 });
