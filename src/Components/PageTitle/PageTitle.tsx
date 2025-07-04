51c18d14c15d3d83d6d05832f709fe8fd926b494
Alyona Pisotska <pisotska.o@gmail.com>
2025-07-04 19:45:27 +0300

    test: - Sentry event for testing

--- src/Components/PageTitle/PageTitle.tsx
+++ src/Components/PageTitle/PageTitle.tsx
@@ -1,24 +1,13 @@
 import { useActiveTrack } from '@/hooks/hooks';
-import { Button } from '@/Components/components';
 
 const PageTitle = () => {
   const { activeTrack } = useActiveTrack();
 
   return (
-    <>
-      <h1 className='text-accent mb-6 font-bold uppercase' data-testid='tracks-header'>
-        Music tracks for every taste!&nbsp;
-        {activeTrack.length > 0 && <span>Active track: {activeTrack}</span>}
-      </h1>
-
-      <Button
-        onClick={() => {
-          throw new Error('test error');
-        }}
-      >
-        Error
-      </Button>
-    </>
+    <h1 className='text-accent mb-6 font-bold uppercase' data-testid='tracks-header'>
+      Music tracks for every taste!&nbsp;
+      {activeTrack.length > 0 && <span>Active track: {activeTrack}</span>}
+    </h1>
   );
 };
 
--- src/integrations/sentry.ts
+++ src/integrations/sentry.ts
@@ -1,18 +1,20 @@
 import * as Sentry from '@sentry/react';
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  release: `${import.meta.env.VITE_SENTRY_PROJECT}-${__APP_VERSION__}`,
-  environment: import.meta.env.MODE,
+if (import.meta.env.MODE === 'production') {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    release: `${import.meta.env.VITE_SENTRY_PROJECT}-${__APP_VERSION__}`,
+    environment: import.meta.env.MODE,
 
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
 
-  replaysSessionSampleRate: 0.0,
-  replaysOnErrorSampleRate: 1.0,
-});
+    replaysSessionSampleRate: 0.0,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
