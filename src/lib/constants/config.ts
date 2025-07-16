7ebb0a51d43e32ccf8f3526cdfa5c0a7c04bf215
Alyona Pisotska <pisotska.o@gmail.com>
2025-07-16 14:05:32 +0300

    refactor: * centralize API and Sentry configs, update CI and environment setup

---
+++ src/lib/constants/config.ts
@@ -0,0 +1,6 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const SENTRY_ORG = import.meta.env.VITE_SENTRY_ORG;
+const SENTRY_PROJECT = import.meta.env.VITE_SENTRY_PROJECT;
+const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
+
+export { API_BASE_URL, SENTRY_ORG, SENTRY_PROJECT, SENTRY_DSN };
--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -1,4 +1,4 @@
-export { API_BASE_URL } from './api-base-url';
+export { API_BASE_URL, SENTRY_ORG, SENTRY_PROJECT, SENTRY_DSN } from './config';
 export { API_ENDPOINTS } from './api-endpoints';
 export { ORDER_BY } from './order-by';
 export { TRACK_TABLE_CELL_IDS } from './track-table-cell-ids';
